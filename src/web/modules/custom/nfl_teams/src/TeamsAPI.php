b6084e83a8ea381e870ea3ded0aa5b59fc13d43e
Marek Pisch <60669435+marekDeveloper@users.noreply.github.com>
2021-03-21 21:31:55 -0600

    TeamsAPI.php - add more URL validation

--- src/web/modules/custom/nfl_teams/src/TeamsAPI.php
+++ src/web/modules/custom/nfl_teams/src/TeamsAPI.php
@@ -64,13 +64,44 @@ class TeamsAPI {
     if (UrlHelper::isValid($this->fullUrl, TRUE)) {
       // valid
       $this->message.= 'API URL seems to be valid (UrlHelper)' . PHP_EOL;
-      return TRUE;
+      // this will continue
     } else {
       // not valid
       $this->fullUrl = FALSE;
       $this->message.= 'API URL does not seem to be valid (UrlHelper)' . PHP_EOL;
       return FALSE;
     }
+
+    // let's try to get headers
+    $file_headers = @get_headers($this->fullUrl);
+    $this->message.= print_r($file_headers, 1);
+    if ($file_headers && strpos($file_headers[0], '200 OK')) {
+      // valid
+      $this->message.= 'API URL seems to be valid (get_headers)' . PHP_EOL;
+      return TRUE;
+    } else {
+      // not valid
+      $this->fullUrl = FALSE;
+      $this->message.= 'API URL does not seem to be valid (get_headers)' . PHP_EOL;
+      return FALSE;
+    }
+
+    // use file_exists() function to check if URL actually exists
+    // TO DO! This will only work when allow_url_fopen?
+    // does not seems to be working for API link provided
+    /*
+    if (file_exists($this->fullUrl)) {
+      // valid
+      $this->message.= 'API URL seems to be valid (file_exists)' . PHP_EOL;
+      return TRUE;
+    } else {
+      // not valid
+      $this->fullUrl = FALSE;
+      $this->message.= 'API URL does not seem to be valid (file_exists)' . PHP_EOL;
+      return FALSE;
+    }
+    */
+
   }
 
   public function validateAPI() {
